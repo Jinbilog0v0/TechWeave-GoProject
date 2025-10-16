81fa600680503640725bc42514e1e3a9148e6a02
rauf-create539 <raufimar44@gmail.com>
2025-10-16 22:46:37 +0800

    Initial commit: Added necessary Folders(Data, Pages, Components) and intalled react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Routes, Route, Link ,NavLink } from "react-router-dom";
+
+
+function App() {
+
+  return (
+    <>
+  <div>
+    
+  </div>
+    </>
+  )
+}
+
+export default App
