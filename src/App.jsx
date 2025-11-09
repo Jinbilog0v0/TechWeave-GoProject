23cea8f29927764555899340fbfeae5f98bd3785
JinBilog0.0 <nealjeanclaro05@gmail.com>
2025-11-10 02:13:32 +0800

    copied files from dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import { Routes, Route, Link, NavLink } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import About from "./Pages/About.jsx";
+import React from "react";
+import "@fontsource/poppins";
+import { Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import LandingPage from "./Pages/LandingPage";
+import AddProject from "./Pages/AddProject";
+import Login from "./Components/Login";
+import Registration from "./Components/Registration";
+import ForgotPassword from "./Components/ForgotPassword";
+import PersonalWorkspace from "./Pages/PersonalWorkspace";
+import CollaborativeWorkspace from "./Pages/CollaborativeWorkspace";
+import ExpenseTrack from "./Pages/ExpenseTrack";
+import Settings from "./Pages/Settings";
 
-export default function App() {
-  return (
-	<div>
-  	<nav>
-    	<NavLink to="/" end>Home</NavLink>
-    	{" | "}
-    	<NavLink to="/about">About</NavLink>
-  	</nav>
 
-  	<Routes>
-    	<Route path="/" element={<Home title="Welcome!" />} />
-    	<Route path="/about" element={<About />} />
-  	</Routes>
-	</div>
-  );
+
+export default function App(){
+	return(
+		<div>
+
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/about" element={<About />} />				
+				<Route path="/addproject" element={<AddProject />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/forgot-password" element={<ForgotPassword />} />
+				<Route path="/register" element={<Registration />} />
+				<Route path="/personal" element={<PersonalWorkspace />} />					
+				<Route path="/collaborative" element={<CollaborativeWorkspace />} />							
+				<Route path="/expense" element={<ExpenseTrack />} />							
+					<Route path="/settings" element={<Settings />} />
+			</Routes>
+
+		</div>
+	)
 }
