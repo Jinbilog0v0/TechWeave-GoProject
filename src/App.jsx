39b03fa98cd22e6ec48fc0bc19e37f9121b30dd7
JinBilog0.0 <nealjeanclaro05@gmail.com>
2025-11-10 01:58:44 +0800

    deleted contents

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,3 @@ export default function App() {
 	</div>
   );
 }
-
-import { useState } from 'react'
-import { Routes, Route, Link ,NavLink } from "react-router-dom";
-
-
-function App() {
-
-  return (
-    <>
-  <div>
-    
-  </div>
-    </>
-  )
-}
-
-export default App
