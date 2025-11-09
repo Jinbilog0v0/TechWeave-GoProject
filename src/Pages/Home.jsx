52a0d8754979581519cd25dec3a0346cb59eef63
Jinbilog0v0 <nealjeanclaro05@gmail.com>
2025-11-10 02:14:42 +0800

    Jin bilog0v0 test (#7)
    * Update README.md for React + Vite template
    
    Removed outdated React + Vite template information and updated formatting.
    
    * deleted contents
    
    * copied files from dev

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import Sidebar from "../Components/Sidebar"
+import HomeCalendar from "../Components/HomeCalendar"
 
-export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+export default function Home() {
+  return <div>
+
+  <div className="fixed left-0 top-0 h-full">
+    <Sidebar />
+  </div>
+
+  <main className="ml-[200px] p-6 min-h-screen">
+    <div>
+      <HomeCalendar />
+    </div>
+
+    <div>
+      {/* Expense Tracker */}
+    </div>
+
+    <div>
+      {/* Status */}
+    </div>
+  </main>
+
+  </div>
 }
