0b9ab3fb3e3ec7d2ce3f58080abb09c126399510
JinBilog0.0 <nealjeanclaro05@gmail.com>
2025-11-25 00:19:42 +0800

    added navigate ('/home');

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -124,6 +124,7 @@ const ProfilePage = () => {
             }
             
             alert("Profile updated successfully!");
+            navigate ('/home');
 
         } catch (error) {
             console.error("Update failed", error);
