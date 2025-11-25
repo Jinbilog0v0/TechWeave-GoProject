ed1e5f735630add67f8a542d95d863c77a309f71
JinBilog0.0 <nealjeanclaro05@gmail.com>
2025-11-25 09:01:07 +0800

    re-added New Project Button [PW]

--- frontend/src/pages/PersonalWorkspace.jsx
+++ frontend/src/pages/PersonalWorkspace.jsx
@@ -126,12 +126,12 @@ const PersonalWorkspace = () => {
 
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-3xl font-bold text-gray-800">Personal Workspace</h2>
-        {/* <Button
+        <Button
           onClick={() => openDialog(null)}
           className="flex items-center space-x-2 px-4 py-2 bg-green-700 text-white rounded hover:bg-green-800 transition"
         >
           New Project
-        </Button> */}
+        </Button>
       </div>
 
       <AddProjectDialog
